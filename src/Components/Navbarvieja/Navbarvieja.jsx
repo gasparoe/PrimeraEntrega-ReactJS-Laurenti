b27db2bbad26f23e33942e932a71ef3fe74777f5
Gaspar Laurenti <laurenti8@hotmail.com>
2023-02-12 20:15:43 -0300

    Inicio de repositorio

---
+++ src/Components/Navbarvieja/Navbarvieja.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Navbarvieja.css';
+import logo from './logo.png';
+
+const Navbarvieja = () => {
+  return (
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark menu">
+            <div class="container-fluid">
+                <a class="navbar-brand p-2" href="./index.html"><img class="menu__logo" src={logo}
+                        alt="logo_menu"/></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="./index.html">Tienda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="./index.html">Empresa</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="./index.html">FAQ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="./index.html">Contacto</a>
+                        </li>
+
+                    </ul>
+                    <a href="#separador">
+                        <i class="fa-solid fa-cart-shopping fa-2x" id="btnVerCarrito"></i>
+                    </a>
+                    <span class="cantidadCarrito">
+                        <p id="cantidadCarrito"></p>
+                    </span>
+                    <p id="cantidadCarrito"></p>
+
+                </div>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbarvieja
